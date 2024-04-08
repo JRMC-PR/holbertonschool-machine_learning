7e596a89d3cd4445dbbf97914b72189777a15a4f
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-04-08 14:35:07 -0400

    update task 4

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -1,10 +1,5 @@
-#!/usr/bin/env python3
-"""adds two arrays element-wise"""
-
-
 def add_arrays(arr1, arr2):
-    """adds tow listby element """
-    if len(arr1) != len(arr2):
-        return None
-    nl = []
-    return [nl.append(arr1[i] + arr2[i]) for i in range(len(arr1))]
+  """adds two lists element-wise"""
+  if len(arr1) != len(arr2):
+    return None
+  return [arr1[i] + arr2[i] for i in range(len(arr1))]
