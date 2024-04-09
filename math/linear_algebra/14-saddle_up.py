6d1bb4f6a2f39aa6e1602ab0f4d552bbe36f42e6
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-04-09 16:46:03 -0400

    Adds task 14

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" matrix multiplication """
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """_summary_
+
+    Args:
+        mat1 (numpy.ndarray): numpy array
+        mat2 (numpy.ndarray): numpy array
+
+    Returns:
+        numpy.ndarray:the multiplication of the matrix _
+    """
+    return np.matmul(mat1, mat2)
