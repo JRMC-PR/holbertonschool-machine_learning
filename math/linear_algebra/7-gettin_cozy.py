5e521dc418db92313072400a684f844e36d00a02
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-04-08 21:23:52 -0400

    Adds task 7

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python3
 """ concat 2D matrices """
-import numpy as np
 
 
 def cat_matrices2D(mat1, mat2, axis=0):
     """ concatenate 2D matrices """
+    # if the number of columns are not equal
     if len(mat1[0]) != len(mat2[0]) and axis == 0:
       return None
-    return np.concatenate((mat1, mat2), axis=axis)
+    # if the number of rows are not equal
+    if len(mat1) != len(mat2) and axis == 1:
+        return None
+
+    CAT = []
+    # concatenate along columns
+    if axis == 0:
+      for i in mat1:
+        CAT.append(i.copy())
+      for j in mat2:
+        CAT.append(j.copy())
+    # concatenate along rows
+    else:
+      for i in range(len(mat1)):
+          CAT.append(mat1[i] + mat2[i])
+    return CAT
