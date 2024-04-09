6130856a88fe04dc353f72324562ee008646aa54
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-04-08 21:38:46 -0400

    Pycodestyle fix

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -3,10 +3,10 @@
 
 
 def cat_matrices2D(mat1, mat2, axis=0):
-    """ concatenate 2D matrices """
+    """concatenate 2D matrices"""
     # if the number of columns are not equal
     if len(mat1[0]) != len(mat2[0]) and axis == 0:
-      return None
+        return None
     # if the number of rows are not equal
     if len(mat1) != len(mat2) and axis == 1:
         return None
@@ -14,12 +14,12 @@ def cat_matrices2D(mat1, mat2, axis=0):
     CAT = []
     # concatenate along columns (vertical stacking)
     if axis == 0:
-      for i in mat1:
-        CAT.append(i.copy())
-      for j in mat2:
-        CAT.append(j.copy())
+        for i in mat1:
+            CAT.append(i.copy())
+        for j in mat2:
+            CAT.append(j.copy())
     # concatenate along rows (horizontal stacking)
     else:
-      for i in range(len(mat1)):
-          CAT.append(mat1[i] + mat2[i])
+        for i in range(len(mat1)):
+            CAT.append(mat1[i] + mat2[i])
     return CAT
