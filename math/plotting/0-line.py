4136731d9002dabf4c27bdc2c41ef2baf3e9797f
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-04-15 20:14:22 -0400

    UPdate taslk 0

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -4,14 +4,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def line():
-    """Plot y as a line graph
-    """
+    """Plot y as a line graph"""
 
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
-    plt.plot(y, 'r-')  # Plot y as a solid red line
+    plt.plot(y, "r-")  # Plot y as a solid red line
     plt.xlim([0, 10])  # Set the x-axis range from 0 to 10
 
     plt.show()  # Display the plot
