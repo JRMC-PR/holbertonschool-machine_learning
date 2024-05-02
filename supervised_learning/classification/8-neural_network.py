4e00396ab385aab5c957605d64f511c7d1c58d78
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-05-02 08:25:50 -0400

    UPpdate3 comments

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -30,9 +30,9 @@ class NeuralNetwork:
         if nodes < 1:
             raise ValueError("nodes must be a positive integer")
         # Initialize weights and biases
-        self.W1 = np.random.randn(nodes, nx)  # Weights for the hidden layer
-        self.b1 = np.zeros((nodes, 1))  # Bias for the hidden layer
+        self.W1 = np.random.randn(nodes, nx)  # Weights of the hidden layer
+        self.b1 = np.zeros((nodes, 1))  # Bias of the hidden layer
         self.A1 = 0  # Activated output for the hidden layer
-        self.W2 = np.random.randn(1, nodes)  # Weights for the output neuron
-        self.b2 = 0  # Bias for the output neuron
-        self.A2 = 0  # Activated output for the output neuron
+        self.W2 = np.random.randn(1, nodes)  # Weights of the output neuron
+        self.b2 = 0  # Bias of the output neuron
+        self.A2 = 0  # Activated output of the output neuron
