29166ceede4c590fd4173b98179679e3d4cbc0d2
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-05-15 08:41:09 -0400

    Adds task 6

---
+++ supervised_learning/optimization/6-momentum.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""This moduel contains the function for creating a momentum
+optimization operation in Tensorflow
+"""
+
+import tensorflow as tf
+
+def create_momentum_op(alpha, beta1):
+    """
+    Set up the gradient descent with momentum
+    optimization algorithm in TensorFlow.
+
+    Parameters:
+    alpha (float): The learning rate.
+    beta1 (float): The momentum weight.
+
+    Returns:
+    tf.keras.optimizers.SGD: The SGD optimizer object with momentum.
+    """
+    # Create a SGD optimizer object with the specified
+    # learning rate and momentum weight
+    optimizer = \
+        tf.keras.optimizers.SGD(learning_rate=alpha, momentum=beta1)
+
+    # Return the optimizer
+    return optimizer
