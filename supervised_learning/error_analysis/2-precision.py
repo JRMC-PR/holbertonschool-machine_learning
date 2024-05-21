d2ae6e778b93bbb0c0d0e15bfaed5559f60a26f3
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-05-21 12:19:18 -0400

    Pycodestyle fix

--- supervised_learning/error_analysis/2-precision.py
+++ supervised_learning/error_analysis/2-precision.py
@@ -6,15 +6,19 @@ import numpy as np
 
 
 def precision(confusion):
-    """This function calculates the precision for each class in a confusion matrix
+    """This function calculates the precision for each
+    class in a confusion matrix
 
     Args:
-        confusion (numpy.ndarray): A confusion matrix of shape (classes, classes)
-        where row indices represent the correct labels and column indices represent
+        confusion (numpy.ndarray): A confusion matrix of
+        shape (classes, classes)
+        where row indices represent the correct labels and
+        column indices represent
         the predicted labels
 
     Returns:
-        numpy.ndarray: A numpy.ndarray of shape (classes,) containing the precision
+        numpy.ndarray: A numpy.ndarray of shape (classes,)
+        containing the precision
         of each class
     """
     return np.diag(confusion) / np.sum(confusion, axis=0)
