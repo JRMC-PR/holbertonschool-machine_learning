add39acf70208623590cb04c33b5315fda751390
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-05-26 17:00:37 -0400

    Fix task 7

--- supervised_learning/regularization/7-early_stopping.py
+++ supervised_learning/regularization/7-early_stopping.py
@@ -10,23 +10,23 @@ def early_stopping(cost, opt_cost, threshold, patience, count):
 
     Parameters:
     cost: float - the current validation cost of the neural network.
-    opt_cost: float - the lowest recorded validation cost of
-    the neural network.
+    opt_cost: float - the lowest recorded validation cost of the
+    neural network.
     threshold: float - the threshold used for early stopping.
     patience: int - the patience count used for early stopping.
     count: int - the count of how long the threshold has not been met.
 
     Returns:
-    A boolean of whether the network should be stopped early,
-    followed by the updated count.
+    A boolean of whether the network should be stopped early, followed
+    by the updated count.
     """
-    # If the current cost is less than the optimal cost plus the threshold,
+    # If the current cost is less than the optimal cost by the threshold,
     # then the count is reset to 0, as the threshold has been met.
-    if cost < opt_cost + threshold:
+    if cost < opt_cost - threshold:
         count = 0
     else:
         # If the current cost is not less than the optimal cost
-        # plus the threshold,
+        # by the threshold,
         # then the count is incremented, as the threshold has not been met.
         count += 1
 
