3edb2e8c95a76a2805a52ee12b4ebf835f2ff7ab
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-05-26 11:30:38 -0400

    pycodestye fix

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -19,5 +19,4 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
     l2_reg = 0
     for i in range(1, L + 1):
         l2_reg += np.linalg.norm(weights['W' + str(i)])
-    return cost + (lambtha / ( 2 * m)) * l2_reg
-
+    return cost + (lambtha / (2 * m)) * l2_reg
