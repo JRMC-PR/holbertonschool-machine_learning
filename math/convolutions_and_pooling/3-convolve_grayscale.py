218d8f02b0e51b75d48e3f52e574acde9b23dfe0
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-06-02 15:16:44 -0400

    Update task 3

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -1,50 +1,53 @@
 #!/usr/bin/env python3
-"""
-Module to perform a convolution on grayscale
-images with custom padding and stride.
-"""
+"""Strided convolution on grayscale images"""
+
 import numpy as np
 
 
-def convolve_grayscale(images, kernel, padding='same',
-                       stride=(1, 1)):
+def convolve_grayscale(images, kernel, padding="same", stride=(1, 1)):
     """
-    Perform a convolution on grayscale images
-    with custom padding and stride.
+    Performs a convolution on grayscale images with custom padding and stride.
 
     Parameters:
-    images (numpy.ndarray): The grayscale images
-    with shape (m, h, w).
-    kernel (numpy.ndarray): The kernel for the
-    convolution with shape (kh, kw).
-    padding (str or tuple): The padding for the
-    height and width of the image.
-    stride (tuple): The stride for the height and
-    width of the image.
+    images (numpy.ndarray): The grayscale images with shape (m, h, w).
+    kernel (numpy.ndarray): The kernel for the convolution with shape (kh, kw).
+    padding (str or tuple): The padding for the height and width of the image.
+    stride (tuple): The stride for the height and width of the image.
 
     Returns:
     A numpy.ndarray containing the convolved images.
     """
+    # Extract dimensions from images' and kernel's shapes
     m, h, w = images.shape
     kh, kw = kernel.shape
     sh, sw = stride
 
-    if isinstance(padding, tuple):
+    # Calculate padding for 'same' and 'valid'
+    if padding == "same":
+        ph = int(((h - 1) * sh - h + kh) / 2) + 1
+        pw = int(((w - 1) * sw - w + kw) / 2) + 1
+    elif padding == "valid":
+        ph, pw = 0, 0
+    else:
         ph, pw = padding
-    elif padding == 'same':
-        ph = max((kh - 1) // 2, kh // 2)
-        pw = max((kw - 1) // 2, kw // 2)
-    else:  # padding == 'valid'
-        ph = pw = 0
 
-    images_padded = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), 'constant')
-    output_h = (h - kh + 2 * ph) // sh + 1
-    output_w = (w - kw + 2 * pw) // sw + 1
+    # Calculate output dimensions
+    output_h = int((h - kh + (2 * ph)) / sh) + 1
+    output_w = int((w - kw + (2 * pw)) / sw) + 1
+
+    # Initialize output with zeros
     output = np.zeros((m, output_h, output_w))
 
-    for x in range(0, output_w * sw, sw):
-        for y in range(0, output_h * sh, sh):
-            output[:, y // sh, x // sw] = \
-            (images_padded[:, y: y + kh, x: x + kw] * kernel).sum(axis=(1, 2))
+    # Pad images
+    image_padded = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), "constant")
+
+    # Apply kernel to images
+    for i in range(output_h):
+        for j in range(output_w):
+            output[:, i, j] = np.sum(
+                image_padded[:, i * sh: i * sh + kh,
+                             j * sw: j * sw + kw] * kernel,
+                axis=(1, 2),
+            )
 
     return output
