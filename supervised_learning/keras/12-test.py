b606c2e4529e59debf448d356b963faa72a3963b
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-06-02 14:16:42 -0400

    Update task 12

--- supervised_learning/keras/12-test.py
+++ supervised_learning/keras/12-test.py
@@ -22,4 +22,4 @@ def test_model(network, data, labels, verbose=True):
     respectively.
     """
     loss, accuracy = network.evaluate(data, labels, verbose=verbose)
-    return loss, accuracy
+    return [loss, accuracy]
