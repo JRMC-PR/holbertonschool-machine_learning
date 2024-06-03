2dcdfb6ec041842549f942cef4251d4b839ac8de
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-06-03 11:36:27 -0400

    Fix import error fo4r task 1

--- supervised_learning/cnn/1-pool_forward.py
+++ supervised_learning/cnn/1-pool_forward.py
@@ -5,8 +5,6 @@ a pooling layer of a neural network
 import numpy as np
 
 
-import numpy as np
-
 def pool_forward(A_prev, kernel_shape, stride=(1, 1), mode='max'):
     """
     Performs forward propagation over a pooling layer of a neural network.
