d2d635199f4e2cb1717352338a52b9238dde68b0
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-06-28 17:01:42 -0400

    UPedate task 1

--- supervised_learning/object_detection/1-yolo.py
+++ supervised_learning/object_detection/1-yolo.py
@@ -74,31 +74,83 @@ class Yolo:
                 probabilities for
                 each output, respectively
         """
+        # List to hold the processed boundary boxes for each output
         boxes = []
+        # List to hold the confidence for each box in each output
         box_confidences = []
+        # List to hold the class probabilities for each box in each output
         box_class_probs = []
 
-        # unpack the output
-        for output in outputs:
+        #Unpack the outputs
+        for i, output in enumerate(outputs):
+            # Ig nore the rest with _
             grid_height, grid_width, anchor_boxes, _ = output.shape
-            box = np.zeros((grid_height, grid_width, anchor_boxes, 4))
+            # Extract the box parameters
+            box = output[..., :4]
+            # Extract the individual components
+            t_x = box[..., 0]
+            t_y = box[..., 1]
+            t_w = box[..., 2]
+            t_h = box[..., 3]
 
-            # Calculate the cx, cy, width and height of the boxes
-            box_xy = sigmoid(output[..., :2])
-            box_wh = tf.exp(output[..., 2:4])
-            box_confidence = sigmoid(output[..., 4:5])
-            box_class_prob = sigmoid(output[..., 5:])
+            # Create 3D grid for the anchor boxes
+            # Create a grid for the x coordinates
+            c_x = np.arange(grid_width).reshape(1, grid_width)
+            #  Repeat the x grid anchor_boxes times
+            c_x = np.repeat(c_x, grid_height, axis=0)
+            # Reshape to add the anchor boxes
+            c_x = np.repeat(c_x[..., np.newaxis], anchor_boxes, axis=2)
 
-            # Calculate the top left and bottom right corners of the boxes
-            box[:, :, :, 0:2] = box_xy - (box_wh / 2.0)  # x1, y1
-            box[:, :, :, 2:4] = box_xy + (box_wh / 2.0)  # x2, y2
+            # Create a grid for the y coordinates
+            c_y = np.arange(grid_width).reshape(1, grid_width)
+            # Repeat the y grid anchor_boxes times
+            c_y = np.repeat(c_y, grid_height, axis=0).T
+            # Reshape to add the anchor boxes
+            c_y = np.repeat(c_y[..., np.newaxis], anchor_boxes, axis=2)
 
-            # Adjust boxes to be relative to the original image size
-            box[:, :, :, 0:4] *= np.tile(image_size, 2) / np.tile(
-                [grid_width, grid_height], 2)
+            # Create a grid for the anchor boxes
+            b_x = (sigmoid(t_x) + c_x) / grid_width
+            b_y = (sigmoid(t_y) + c_y) / grid_height
 
+            anchor_width = self.anchors[i, :, 0]
+            anchor_height = self.anchors[i, :, 1]
+
+            image_width = self.model.input.shape[1]
+            image_height = self.model.input.shape[2]
+            b_w = (anchor_width * np.exp(t_w)) / image_width
+            b_h = (anchor_height * np.exp(t_h)) / image_height
+
+            # top left corner
+            x1 = (b_x - b_w / 2)
+            y1 = (b_y - b_h / 2)
+
+            # bottom right corner
+            x2 = (b_x + b_w / 2)
+            y2 = (b_y + b_h / 2)
+
+            # box coordinate relative to the image size
+            x1 = x1 * image_size[1]
+            y1 = y1 * image_size[0]
+            x2 = x2 * image_size[1]
+            y2 = y2 * image_size[0]
+
+            # Update boxes
+            box[..., 0] = x1
+            box[..., 1] = y1
+            box[..., 2] = x2
+            box[..., 3] = y2
+
+            # Append the box to the boxes list
             boxes.append(box)
+
+            # Extract the box confidence and aply sigmoid
+            box_confidence = sigmoid(output[..., 4:5])
             box_confidences.append(box_confidence)
+
+
+            # Extract the box class probabilities and aply sigmoid
+            box_class_prob = sigmoid(output[..., 5:])
             box_class_probs.append(box_class_prob)
 
         return boxes, box_confidences, box_class_probs
+
