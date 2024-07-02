87dd15f0abb0e867bfb2c69cc5d8176c786c09fa
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-07-02 14:32:04 -0400

    bug fix

--- supervised_learning/neural_style_transfer/0-neural_style.py
+++ supervised_learning/neural_style_transfer/0-neural_style.py
@@ -27,11 +27,11 @@ class NST:
             alpha {float} -- the weight for style cost
             beta {float} -- the weight for content cost
         """
-        # if not isinstance(style_image, np.ndarray) or style_image.ndim != 3:
-        #     error1 = "style_image must be a numpy.ndarray with shape (h, w, 3)"
-        #     raise TypeError(error1)
-        if style_image.shape[-1] != 3:
+        if not isinstance(style_image, np.ndarray) or style_image.ndim != 3:
+            error1 = "style_image must be a numpy.ndarray with shape (h, w, 3)"
             raise TypeError(error1)
+        # if style_image.shape[-1] != 3:
+        #     raise TypeError(error1)
         if not isinstance(content_image,
                           np.ndarray) or content_image.ndim != 3:
             error2 = "content_image must be "
