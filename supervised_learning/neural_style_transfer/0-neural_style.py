3741a3fd4b04ee66826f3d8f7759886f8db60b5a
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-07-02 13:37:36 -0400

    Update task 0

--- supervised_learning/neural_style_transfer/0-neural_style.py
+++ supervised_learning/neural_style_transfer/0-neural_style.py
@@ -28,12 +28,17 @@ class NST:
             beta {float} -- the weight for content cost
         """
         if not isinstance(style_image, np.ndarray) or style_image.ndim != 3:
-            raise TypeError("style_image must be \
-                            numpy.ndarray with shape (h, w, 3)")
-        if not isinstance(content_image, np.ndarray) or content_image.ndim != 3:
-            raise TypeError("content_image must be \
-                            numpy.ndarray with shape (h, w, 3)")
-        if not (isinstance(alpha, (float, int)) and  alpha >= 0):
+            raise TypeError(
+                "style_image must be \
+                            numpy.ndarray with shape (h, w, 3)"
+            )
+        if not isinstance(content_image,
+                          np.ndarray) or content_image.ndim != 3:
+            raise TypeError(
+                "content_image must be \
+                            numpy.ndarray with shape (h, w, 3)"
+            )
+        if not (isinstance(alpha, (float, int)) and alpha >= 0):
             raise TypeError("alpha must be a non-negative number")
         if not (isinstance(beta, (float, int)) and beta >= 0):
             raise TypeError("beta must be a non-negative number")
@@ -51,14 +56,17 @@ class NST:
         Returns:
             np.ndarray -- the scaled image
         """
-        if not isinstance(image, np.ndarray) or image.ndim != 3:
-            raise TypeError("image must be a numpy.ndarray with shape (h, w, 3)")
+        if not isinstance(image,
+                          np.ndarray) or image.ndim != 3 or
+        image.shape[-1] != 3:
+            raise TypeError("image must be a \
+                            numpy.ndarray with shape (h, w, 3)")
         max_dim = 512
         h, w, _ = image.shape
         scale = max_dim / max(h, w)
         h_new, w_new = int(h * scale), int(w * scale)
         image = tf.convert_to_tensor(image, dtype=tf.float32)
-        image = tf.image.resize(image, [h_new, w_new], method='bicubic')
+        image = tf.image.resize(image, [h_new, w_new], method="bicubic")
         image /= 255.0
         image = tf.clip_by_value(image, 0, 1)
         return tf.expand_dims(image, axis=0)
