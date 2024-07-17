80a5e1580d04f2f7be70bda977c65a469402d1ea
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-07-17 16:17:52 -0400

    adjugate task

---
+++ math/advanced_linear_algebra/3-adjugate.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""This module calculates the adjugate matrix
+of a square matrix
+"""
+minor = __import__('1-minor').minor
+cofactor = __import__('2-cofactor').cofactor
+
+
+def adjugate(matrix):
+    """
+    Args:
+        matrix: list of lists whose adjugate matrix should be calculated
+    Returns:
+        the adjugate matrix of a matrix
+    """
+    # Check if matrix is a list of lists
+    if not isinstance(matrix, list):
+        raise TypeError("matrix must be a list of lists")
+    if not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a list of lists")
+
+    if len(matrix) == 0:
+        raise ValueError("matrix must be a list of lists")
+
+    # calculate the minor matrix
+    minor_matrix = minor(matrix)
+    # calculate the cofactor matrix
+    cofactor_matrix = cofactor(minor_matrix)
+    # calculate the adjugate matrix
+    adjugate_matrix =[list(row) for row in zip(*cofactor_matrix)]
+    return adjugate_matrix
