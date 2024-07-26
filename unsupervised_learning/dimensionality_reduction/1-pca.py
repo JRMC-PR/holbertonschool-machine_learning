a9b8665eadb8cdb72fd4bf3a4e8376f692c1d25b
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-07-26 13:30:10 -0400

    Adds task 1

---
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""This modlue contains the function pca(X, ndim)"""
+import numpy as np
+
+
+def pca(X, ndim):
+    """This function performs PCA on a dataset
+    Args:
+        X: numpy.ndarray of shape (n, d) where:
+            - n is the number of data points
+            - d is the number of dimensions in each point
+        ndim: the new dimensionality of the transformed X
+        Returns:
+        T: numpy.ndarray of shape (n, ndim) containing the transformed version
+        of X
+        """
+    # Ensure all dimetions have a mean between all data points
+    X = X - np.mean(X, axis=0)
+
+    # Compute the SVD:
+    U, S, Vt = np.linalg.svd(X)
+
+    # Compute the cumulative sum of the explained variance ratio
+    tr = np.matmul(U[..., :ndim], np.diag(S[:ndim]))
+
+    return tr
