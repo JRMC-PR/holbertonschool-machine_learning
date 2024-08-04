17c88fc41be5d95ab21517003a2a1862891fda89
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-08-04 19:29:11 -0400

    add changes to task 9

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -53,33 +53,62 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
     # Step 2: Extract the shape from X
     n, d = X.shape
 
-    # Step 3: Initialize the variables
-    likelihoods = []
-    pis = []
-    ms = []
-    Ss = []
-    bys = []
+    # # Step 3: Initialize the variables
+    # likelihoods = []
+    # pis = []
+    # ms = []
+    # Ss = []
+    # bys = []
 
-    # Step 4: Perform the EM algorithm fr each cluster size
-    for i in range(kmin, kmax + 1):
-        pi, m, S, g, likelihood = expectation_maximization(
-            X, i, iterations, tol, verbose)
-        if pi is None:
-            return None, None, None, None
-        pis.append(pi)
-        ms.append(m)
-        Ss.append(S)
-        likelihoods.append(likelihood)
+    # # Step 4: Perform the EM algorithm fr each cluster size
+    # for i in range(kmin, kmax + 1):
+    #     pi, m, S, g, likelihood = expectation_maximization(
+    #         X, i, iterations, tol, verbose)
+    #     if pi is None:
+    #         return None, None, None, None
+    #     pis.append(pi)
+    #     ms.append(m)
+    #     Ss.append(S)
+    #     likelihoods.append(likelihood)
 
-        # Calculate the BIC
-        p = (i * d * (d + 1) / 2) + (d * i) + (i - 1)
-        bic = p * np.log(n) - 2 * likelihood
-        bys.append(bic)
+    #     # Calculate the BIC
+    #     p = (i * d * (d + 1) / 2) + (d * i) + (i - 1)
+    #     bic = p * np.log(n) - 2 * likelihood
+    #     bys.append(bic)
 
-    # Step 5: Find the best number of clusters
-    likelihoods = np.array(likelihoods)
-    bys = np.array(bys)
-    best_k = np.argmin(bys)
-    best_result = (pis[best_k], ms[best_k], Ss[best_k])
+    # # Step 5: Find the best number of clusters
+    # likelihoods = np.array(likelihoods)
+    # bys = np.array(bys)
+    # best_k = np.argmin(bys)
+    # best_result = (pis[best_k], ms[best_k], Ss[best_k])
 
-    return best_k+1, best_result, likelihoods, bys
+    # return best_k+1, best_result, likelihoods, bys
+
+        # Define pi_t, m_t, S_t: arrays containing the relevant
+    # parameters for all the clusters
+    all_pis = []
+    all_ms = []
+    all_Ss = []
+    all_lkhds = []
+    all_bs = []
+
+    # Iterate over the ((kmax + 1) - kmin) clusters
+    for k in range(kmin, kmax + 1):
+        pi, m, S, g, lkhd = expectation_maximization(X, k, iterations,
+                                                     tol, verbose)
+        all_pis.append(pi)
+        all_ms.append(m)
+        all_Ss.append(S)
+        all_lkhds.append(lkhd)
+        # p: the number of parameters required for the model
+        p = (k * d * (d + 1) / 2) + (d * k) + (k - 1)
+        # b: array containing the BIC value for each cluster size tested
+        b = p * np.log(n) - 2 * lkhd
+        all_bs.append(b)
+
+    all_lkhds = np.array(all_lkhds)
+    all_bs = np.array(all_bs)
+    best_k = np.argmin(all_bs)
+    best_result = (all_pis[best_k], all_ms[best_k], all_Ss[best_k])
+
+    return best_k+1, best_result, all_lkhds, all_bs
