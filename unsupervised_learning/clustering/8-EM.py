1b23f16f39d4f0b3d9df0ea5cc202d9b14af3429
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-08-04 19:48:14 -0400

    update task 8 to xcheck for succes on task 9

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -65,11 +65,11 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
         # Print the log likelihood
         if verbose:
             # Print the log likelihood every 10 iterations
-            if i % 10 == 0 or i == iterations or np.abs(
+            if i % 10 == 0 or i == iterations or abs(
                     likelihood - l_prev) <= tol:
                 print(f"Log Likelihood after {i} iterations: {likelihood:.5f}")
         # Check fr convergence
-        if np.abs(likelihood - l_prev) < tol:
+        if abs(likelihood - l_prev) < tol:
             break
         l_prev = likelihood
 
