624523aab70c1999dbb0a062554542fdd8e77e7b
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-09-20 09:38:13 -0400

    Update task 3

--- supervised_learning/word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -9,4 +9,12 @@ def gensim_to_keras(model):
     Returns:
         the trainable keras Embedding
     """
-    return model.wv.get_keras_embedding(train_embeddings=True)
+    # Extract the word vectors from the model
+    weights = model.wv.vectors
+    embedding_layer= tf.keras.layers.Embedding(
+        input_dim=weights.shape[0],
+        output_dim=weights.shape[1],
+        weights=[weights],
+        trainable=True
+    )
+    return embedding_layer
