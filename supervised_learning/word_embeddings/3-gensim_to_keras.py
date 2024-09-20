c39396bd8d873ae2d9533c8854b04adc532e7eb2
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-09-20 09:42:03 -0400

    Pycode Style fix

--- supervised_learning/word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -2,8 +2,10 @@
 """This module has the method gensim_to_keras"""
 import tensorflow as tf
 
+
 def gensim_to_keras(model):
-    """This method converts a gensim word2vec model to a keras Embedding layer
+    """This method converts a gensim word2vec model to a
+    keras Embedding layer
     Args:
         model is a trained gensim word2vec models
     Returns:
@@ -11,7 +13,8 @@ def gensim_to_keras(model):
     """
     # Extract the word vectors from the model
     weights = model.wv.vectors
-    embedding_layer= tf.keras.layers.Embedding(
+    # crerate the keras embedding layer
+    embedding_layer = tf.keras.layers.Embedding(
         input_dim=weights.shape[0],
         output_dim=weights.shape[1],
         weights=[weights],
