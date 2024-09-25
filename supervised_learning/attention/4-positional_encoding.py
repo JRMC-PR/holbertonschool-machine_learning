19739ff1d2508d785d14fba4ebfcfc337d45a938
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-09-25 12:08:50 -0400

    pycode style fix for task 3

--- supervised_learning/attention/4-positional_encoding.py
+++ supervised_learning/attention/4-positional_encoding.py
@@ -20,11 +20,11 @@ def positional_encoding(max_seq_len, dm):
     # Compute the angles for each position and dimension
     for pos in range(max_seq_len):
         for i in range(dm):
-            angle_rads[pos, i] = pos / np.power(10000, (2 * (i // 2)) / dm)
+            angle_rads[pos, i] = pos / np.power(
+                10000, (2 * (i // 2)) / dm)
 
     # Apply sine to even indices (2i) and cosine to odd indices (2i+1)
     angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
     angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
-
 
     return angle_rads
