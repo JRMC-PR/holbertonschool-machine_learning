e830d13f1ee36d8429b5290506b9747b7ce487b0
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-10-10 16:16:50 -0400

    Adds task 1

--- supervised_learning/qa_bot/0-qa.py
+++ supervised_learning/qa_bot/0-qa.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import tensorflow as tf
-import tensorflow_hub as hub
-from transformers import BertTokenizer
+import tensorflow as tf
+from transformers import BertTokenizer, TFBertForQuestionAnswering
 
 
 def question_answer(question, reference):
@@ -9,28 +9,35 @@ def question_answer(question, reference):
     Finds a snippet of text within a reference document to answer a question.
 
     :param question: str, containing the question to answer
-    :param reference: str, containing the reference document from which to
-    find the answer
+    :param reference: str, containing the reference document from which
+    to find the answer
     :return: str, containing the answer or None if no answer is found
     """
     # Load the pre-trained BERT tokenizer
     tokenizer = BertTokenizer.from_pretrained(
-        'bert-large-uncased-whole-word-masking-finetuned-squad')
+        "bert-large-uncased-whole-word-masking-finetuned-squad"
+    )
 
     # Load the BERT model for question answering
-    model = hub.load("https://tfhub.dev/see--/bert-uncased-tf2-qa/1")
+    model = TFBertForQuestionAnswering.from_pretrained(
+        "bert-large-uncased-whole-word-masking-finetuned-squad"
+    )
 
     # Tokenize the input question and reference
     inputs = tokenizer.encode_plus(
-        question, reference, add_special_tokens=True, return_tensors="tf")
+        question, reference, add_special_tokens=True, return_tensors="tf"
+    )
 
-    # Get the input IDs and attention mask
+    # Get the input IDs, attention mask, and token type IDs
     input_ids = inputs["input_ids"]
     attention_mask = inputs["attention_mask"]
+    token_type_ids = inputs["token_type_ids"]
 
     # Make predictions using the BERT model
-    outputs = model([input_ids, attention_mask])
-    start_logits, end_logits = outputs["start_logits"], outputs["end_logits"]
+    outputs = model(
+        input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids
+    )
+    start_logits, end_logits = outputs.start_logits, outputs.end_logits
 
     # Find the start and end positions of the answer
     start_index = tf.argmax(start_logits, axis=1).numpy()[0]
@@ -38,7 +45,8 @@ def question_answer(question, reference):
 
     # Extract the answer from the reference document
     if start_index <= end_index:
-        answer_tokens = input_ids[0, start_index:end_index + 1]
+        answer_tokens = input_ids[
+            0, start_index: end_index + 1]
         answer = tokenizer.decode(answer_tokens)
     else:
         answer = None
