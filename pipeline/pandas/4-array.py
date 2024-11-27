c5d9c5170b553775cf42c91ee7d24f935fd1769e
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-11-27 00:16:48 +0000

    Adds task 4

---
+++ pipeline/pandas/4-array.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""This module converts a dataframe to a numpy array"""
+import pandas as pd
+
+
+def array(df):
+    """This function converts a dataframe to a numpy array.
+
+    Args:
+        df: the dataframe to convert
+
+    Returns:
+        The numpy array
+    """
+    # Return the last 10 rows of the columns 'High' and 'Close'
+    # as a numpy array
+    return df[['High', 'Close']].tail(10).to_numpy()
