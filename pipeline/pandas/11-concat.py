83562efed9785a62f4ac0b36ab4771d29684dbe9
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-11-28 02:47:14 +0000

    Update task 9

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -13,22 +13,12 @@ def concat(df1, df2):
     Returns:
         The concatenated dataframe
     """
-    # Remove the "Weighted_Price" column
-    if "Weighted_Price" in df.columns:
-        df = df.drop(columns=["Weighted_Price"])
+    # Index both DataFrames on their Timestamp column
+    df1 = index(df1)
+    df2 = index(df2)
 
-    # Fill missing values in the "Close" column with the previous row's value
-    if "Close" in df.columns:
-        df["Close"] = df["Close"].ffill()
+    # Filter the second DataFrame up to the specified Timestamp
+    df2 = df2.loc[:1417411920]
 
-    # Fill missing values in these columns with corresponding "Close" values
-    for column in ["High", "Low", "Open"]:
-        if column in df.columns:
-            df[column] = df[column].fillna(df["Close"])
-
-    # Set missing values in the Volume columns to 0
-    for column in ["Volume_(BTC)", "Volume_(Currency)"]:
-        if column in df.columns:
-            df[column] = df[column].fillna(0)
-
-    return df
+    # NOTE concatenated dataframes, using keys to differentiate data origin
+    return pd.concat([df2, df1], keys=["bitstamp", "coinbase"])
