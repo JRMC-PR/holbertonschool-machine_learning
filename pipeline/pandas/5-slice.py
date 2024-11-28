26e83ecaaa6eacf7abfa8fd524ebeebff25b0b62
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-11-28 01:55:53 +0000

    Adds task 6

--- pipeline/pandas/5-slice.py
+++ pipeline/pandas/5-slice.py
@@ -2,3 +2,17 @@
 """This module slices a dataframe along the
 columns High and Close"""
 import pandas as pd
+
+
+def slice(df):
+    """This function slices a dataframe along the
+    columns High and Close.
+
+    Args:
+        df: the dataframe to slice
+
+    Returns:
+        The sliced dataframe
+    """
+    # Return the columns 'High' and 'Close'
+    return df[['High', 'Low', 'Close', 'Volume_(BTC)']].iloc[::50]
