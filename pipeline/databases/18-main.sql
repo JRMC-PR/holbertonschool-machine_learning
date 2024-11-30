887226b6b658cd94422bfc789a20e663116b9cfe
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-11-30 00:40:21 +0000

    Adds compleated task s

--- pipeline/databases/18-main.sql
+++ pipeline/databases/18-main.sql
@@ -1,1 +1,14 @@
 -- SQL script for 18-main.sql
+SELECT * FROM users;
+
+UPDATE users SET valid_email = 1 WHERE email = "bob@dylan.com";
+UPDATE users SET email = "sylvie+new@dylan.com" WHERE email = "sylvie@dylan.com";
+UPDATE users SET name = "Jannis" WHERE email = "jeanne@dylan.com";
+
+SELECT "--";
+SELECT * FROM users;
+
+UPDATE users SET email = "bob@dylan.com" WHERE email = "bob@dylan.com";
+
+SELECT "--";
+SELECT * FROM users;
--- pipeline/databases/20-main.sql
+++ pipeline/databases/20-main.sql
@@ -1,1 +1,9 @@
 -- SQL script for 20-main.sql
+SELECT * FROM users;
+SELECT * FROM corrections;
+
+SELECT "--";
+CALL ComputeAverageScoreForUser((SELECT id FROM users WHERE name = "Jeanne"));
+
+SELECT "--";
+SELECT * FROM users;
