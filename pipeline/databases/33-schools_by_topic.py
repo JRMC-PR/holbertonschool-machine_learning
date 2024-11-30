4c4ee2668706da3327c2c3afb0efb7d69643d2a5
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-11-30 00:53:00 +0000

    Adds remainig tasks

--- pipeline/databases/33-schools_by_topic.py
+++ pipeline/databases/33-schools_by_topic.py
@@ -1,1 +1,19 @@
-# Python script for 33-schools_by_topic.py
+#!/usr/bin/env python3
+"""
+Returns the list of schools having a specific topic
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Returns the list of schools having a specific topic.
+
+    Args:
+        mongo_collection: The pymongo collection object.
+        topic (str): The topic to search for.
+
+    Returns:
+        A list of documents where the 'topics' field contains
+        the given topic.
+    """
+    return list(mongo_collection.find({"topics": topic}))
